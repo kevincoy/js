37911747d38ee27ec1dbab358357589168862327
Kevin Coy <kevin.coy@sportngin.com>
2013-10-15 10:57:28 -0500

    new file

---
+++ loopsArrays3.js
@@ -0,0 +1,12 @@
+// Print largest number
+
+var array = [3, 6, 2, 56, 32, 5, 89, 32];
+var largest = 0;
+// Write your code below!
+for (var i = 0; i < array.length; i++) {
+    if (array[i] > largest) {
+    largest = array[i];
+    }
+}
+
+console.log(largest);
