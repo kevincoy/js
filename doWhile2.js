72f9e5fee45ef418e3877f27c83fbb05d5ca1be0
Kevin Coy <kevin.coy@sportngin.com>
2013-10-15 14:20:31 -0500

    new file

---
+++ doWhile2.js
@@ -0,0 +1,10 @@
+snake = false;
+
+var getToDaChoppa = function() {
+  do {
+    console.log("Seriously, get to da choppa! Thankfully doc says my tumor results came back as ... " + String(snake) + "?!");
+} while (snake);
+  
+};
+
+getToDaChoppa();
---
+++ doWhileFor.js
@@ -0,0 +1,14 @@
+snake = false;
+
+var getToDaChoppa = function() {
+  do {
+    console.log("Seriously, get to da choppa! Thankfully doc says my tumor results came back as ... " + String(snake) + "?!");
+} while (snake);
+
+for (var i = 100; i > 0; i-=5) {
+	console.log(i);
+}
+  
+};
+
+getToDaChoppa();
