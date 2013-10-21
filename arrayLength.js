9ee7bf95243144bdc7a5089c9eaa1b7fe12f505b
Kevin Coy <kevin.coy@sportngin.com>
2013-10-21 10:09:24 -0500

    new file

---
+++ arrayLength.js
@@ -0,0 +1,6 @@
+// Log all 5 languages to the console.
+
+var languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"];
+for (var i = 0; i < languages.length; i++) {
+    console.log(languages[i]);
+}
